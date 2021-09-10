9ee20467ac276f2f27ef6567bef11efdeaf7d0a2
mohammad badangiz <mbz3a1998@gmail.com>
2021-09-10 07:27:27 +0430

    last version
    last version

---
+++ captcha.js
@@ -0,0 +1,19 @@
+let mbz_letterBanks = "QWm12FERzxcvbnJop34KLZXCVkertyTYUluGHi56789BNMqwIOPsdfASDaghj";
+let mbz_parentElemnt = document.getElementById('captcha') ;
+let mbz_get_head = document.getElementsByTagName('head')[0];
+let creat_css_links = document.createElement('link');
+creat_css_links.rel="stylesheet";
+creat_css_links.href=;
+
+let mbz_catcha = "" ;
+for(var i=0 ; i <6 ; i++){
+    let mbz_index = Math.floor(Math.random()*mbz_letterBanks.length);
+    var mbz_spans = document.createElement('span');
+    mbz_spans.textContent = mbz_letterBanks[mbz_index] ;
+    mbz_spans.style.color = "#ff"+Math.floor(Math.random()*10000) ;
+
+    mbz_parentElemnt.appendChild(mbz_spans);
+    mbz_catcha += mbz_letterBanks[mbz_index];    
+}
+
+mbz_parentElemnt.setAttribute('data-captcha',mbz_catcha );
